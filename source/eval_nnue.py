c46d0e50ad9f8d5fb3aa8438cb1e4ab53d5797dc
Yasushi Sakita <yasushi.sakita@gmail.com>
2020-08-01 19:03:21 +0900

    pythonとC++でNNUEの速度計測してみたら100倍の速さになった。

--- source/eval_nnue.py
+++ source/eval_nnue.py
@@ -8,9 +8,10 @@ yaneOu = soLib.YaneuraOu()
 eval = soLib.py_nnue_eval
 
 sfen = "l6nl/5+P1gk/2np1S3/p1p4Pp/3P2Sp1/1PPb2P1P/P5GS1/R8/LN4bKL w GR5pnsg 1".encode('utf-8')
+import time
+start_time = time.time()
 x = eval(yaneOu, ctypes.create_string_buffer(sfen))
+print("--- %s seconds ---" % (time.time() - start_time))
 print(x)
 
-# str = "l6nl/5+P1gk/2np1S3/p1p4Pp/3P2Sp1/1PPb2P1P/P5GS1/R8/LN4bKL w GR5pnsg 1"
-# b_string = str.encode('utf-8')
-# soLib.string_test(ctypes.create_string_buffer(b_string))
+# soLib.string_test(ctypes.create_string_buffer(sfen))
