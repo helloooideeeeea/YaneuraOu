1f3141fcf687c567a67fc1c4ad39d5c321c0a35c
Yasushi Sakita <yasushi.sakita@gmail.com>
2020-09-04 17:29:53 +0900

    共有ライブラリとして、C++からでもメソッドを呼び出せるようにした。

--- source/python_eval.cpp
+++ source/python_eval.cpp
@@ -1,8 +1,30 @@
-
+#pragma once
 #include "python_eval.h"
 #include <iostream>
 #include <stdio.h>
+#include "search.h"
+#include "thread.h"
+#include "tt.h"
+#include "usi.h"
+#include "misc.h"
+
 using namespace std;
+
+PythonBind::PythonBind() {
+    USI::init(Options);
+    Bitboards::init();
+    Position::init();
+    Search::init();
+    Threads.set(Options["Threads"]);
+    Eval::init();
+}
+
+int PythonBind::nnue_eval(std::string sfen) {
+    Position pos;
+    StateListPtr states(new StateList(1));
+    pos.set(sfen, &states->back(), Threads.main());
+    return static_cast<int>(Eval::compute_eval(pos));
+}
 
 extern "C" {
 
--- source/python_eval.h
+++ source/python_eval.h
@@ -1,27 +1,7 @@
-#pragma once
-#include "search.h"
-#include "thread.h"
-#include "tt.h"
-#include "usi.h"
-#include "misc.h"
 #include <iostream>
-
 class PythonBind {
 public:
-    PythonBind() {
-        USI::init(Options);
-        Bitboards::init();
-        Position::init();
-        Search::init();
-        Threads.set(Options["Threads"]);
-        Eval::init();
-    }
-
-    int nnue_eval(std::string sfen) {
-        Position pos;
-        StateListPtr states(new StateList(1));
-        pos.set(sfen, &states->back(), Threads.main());
-        return static_cast<int>(Eval::compute_eval(pos));
-    }
+    PythonBind();
+    int nnue_eval(std::string sfen);
 };
 
